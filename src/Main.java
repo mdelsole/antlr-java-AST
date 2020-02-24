3368cfaefa26244d57b10f622df46608b1f0bd2f
mdelsole <mdelsole64@gmail.com>
2020-02-24 13:39:30 -0500

    Updated main

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,16 @@ public class Main {
 
         System.out.println("parsing: " + args[0]);
 
-        //pascalLexer lexer = new pascalLexer(new ANTLRFileStream(args[0]));
+        // Set args[0] as the input to our lexer
+        pascalLexer lexer = new pascalLexer(new ANTLRFileStream(args[0]));
+        // Set the lexed file as input to our parser
+        pascalParser parser = new pascalParser(new CommonTokenStream(lexer));
+        // Create a parse tree. Program() is our entry point
+        ParseTree tree = parser.program();
+        // Our visitor (does the actions as tree is traversed)
+        PascalVisitor visitor = new PascalVisitor();
+        // We need to implement this:
+        // visitor.visit(tree);
 
     }
 }
